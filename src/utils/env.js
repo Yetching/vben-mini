3c770946d074555e24098afb696e18028e3cab4a
Yetching <840850332@qq.com>
2021-04-12 22:27:27 +0800

    晚间练习

---
+++ src/utils/env.js
@@ -0,0 +1,51 @@
+import { warn } from '/@/utils/log.js'
+import pkg from '../../package.json'  //json文件就是一个对象
+import { getConfigFileName } from '../../build/getConfigFileName.js'
+
+export function getCommonStoragePrefix() {
+    const { VITE_GLOB_APP_SHORT_NAME}  = getAppEnvConfig()
+    return `${VITE_GLOB_APP_SHORT_NAME}__${getEnv()}`.toUpperCase()
+}
+
+export function getStorageShortName() {
+    return `${getCommonStoragePrefix()}${`__${pkg.version}`}__`.toUpperCase()
+}
+
+export function getAppEnvConfig() {
+    const ENV_NAME = getConfigFileName(import.meta.env)
+    const ENV = import.meta.env.DEV ? import.meta.env : window[ENV_NAME]
+    const {
+        VITE_GLOB_APP_TITLE,
+        VITE_GLOB_API_URL,
+        VITE_GLOB_APP_SHORT_NAME,
+        VITE_GLOB_API_URL_PREFIX,
+        VITE_GLOB_UPLOAD_URL
+    } = ENV
+
+    if (!/[a-zA-Z\_]*/.test(VITE_GLOB_APP_SHORT_NAME)) {
+        warn(
+            `VITE_GLOB_APP_SHORT_NAME Variables can only be characters/underscores, please modify in the environment variables and re-running.`
+        );
+    }
+    return {
+        VITE_GLOB_APP_TITLE,
+        VITE_GLOB_API_URL,
+        VITE_GLOB_APP_SHORT_NAME,
+        VITE_GLOB_API_URL_PREFIX,
+        VITE_GLOB_UPLOAD_URL,
+    };
+}
+
+export const prodMode = 'production'
+
+export function getEnv() {
+    return import.meta.env.MODE
+}
+
+export function isDevMode() {
+    return import.env.DEV
+}
+
+export function isProdMode() {
+    return import.meta.env.PROD
+}
